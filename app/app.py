b2c278a34e9743d488888adce0982e616a2573ec
Paul Morales <paulmorales@pauls-mbp.mynetworksettings.com>
2026-04-19 18:08:35 -0400

    loading and preparing data for streamlit webapp

---
+++ app/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+# ── Load & prepare data ──────────────────────────────────────────────────────
+
